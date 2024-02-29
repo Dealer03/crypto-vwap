eb73de26a40069b10fccf20069236df3d9cf8c23
Efrain Palencia <epalen6@wgu.edu>
2024-02-29 10:21:13 -0500

    Improves usability workflow

--- crypto_vwap/accounts/static/accounts/sort_table.js
+++ crypto_vwap/accounts/static/accounts/sort_table.js
@@ -1,7 +1,8 @@
 function sortTable(columnIndex) {
-    let table, rows, switching, i, x, y, shouldSwitch;
+    let table, rows, switching, i, x, y, shouldSwitch, ascending;
     table = document.getElementById("transactionTable");
     switching = true;
+    ascending = true; // Default sorting order is ascending
     while (switching) {
         switching = false;
         rows = table.rows;
@@ -9,14 +10,28 @@ function sortTable(columnIndex) {
             shouldSwitch = false;
             x = rows[i].getElementsByTagName("TD")[columnIndex];
             y = rows[i + 1].getElementsByTagName("TD")[columnIndex];
-            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-                shouldSwitch= true;
-                break;
+            // Compare values based on sorting order
+            if (ascending) {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    shouldSwitch= true;
+                    break;
+                }
+            } else {
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    shouldSwitch= true;
+                    break;
+                }
             }
         }
         if (shouldSwitch) {
             rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
             switching = true;
+        } else {
+            if (ascending) {
+                // If no switching occurred and it's the first pass, switch to descending order
+                ascending = false;
+                switching = true;
+            }
         }
     }
 }
