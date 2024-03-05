ef18928cb38d124cf735bd1ae78e9fea0c1758de
Efrain Palencia <epalen6@wgu.edu>
2024-03-05 10:54:26 -0500

    Railway.app setup

--- crypto_vwap/crypto_vwap/settings.py
+++ crypto_vwap/crypto_vwap/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^+=)fk1&z!(jb#s0x7fq7qs#q_#!f#2etac&424dnuh=cx*6#o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -124,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
