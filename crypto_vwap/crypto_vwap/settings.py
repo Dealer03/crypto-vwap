f0a4a242da258ae34a730fbf688e4ec4d206e6e5
Efrain Palencia <epalen6@wgu.edu>
2024-03-05 23:00:13 -0500

    Testing deployment

--- crypto_vwap/crypto_vwap/settings.py
+++ crypto_vwap/crypto_vwap/settings.py
@@ -21,15 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^+=)fk1&z!(jb#s0x7fq7qs#q_#!f#2etac&424dnuh=cx*6#o'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
 CCRF_TRUSTED_ORIGINS = [
-    ''
+    'crypto-vwap-production.up.railway.app'
 ]
 
 
