89048c9bb4c8c859774f40c28e90a23dc4290a08
Efrain Palencia <epalen6@wgu.edu>
2024-03-07 10:24:02 -0500

    change html encoding to ANSI

--- crypto_vwap/html_parser/utils/parse.py
+++ crypto_vwap/html_parser/utils/parse.py
@@ -2,22 +2,11 @@ import csv
 from bs4 import BeautifulSoup
 
 
-import chardet
-
-
 def parse_html_to_csv(html_file_path, csv_file_path):
-    # Detect the encoding of the HTML file
-    with open(html_file_path, 'rb') as html_file:
-        rawdata = html_file.read()
-        encoding_result = chardet.detect(rawdata)
-        encoding = encoding_result['encoding']
-
-    # Open the HTML file with detected encoding
-    with open(html_file_path, 'r', encoding=encoding) as html_file:
+    with open(html_file_path, 'r', encoding='utf-8') as html_file:
         soup = BeautifulSoup(html_file, 'html.parser')
         table = soup.find('table')
 
-        # Open the CSV file with ANSI encoding
         with open(csv_file_path, 'w', newline='', encoding='ANSI') as csv_file:
             writer = csv.writer(csv_file)
             # Write header row
